9709c10b3e17813ebecb2a320e620e903f1bd0cf
adefitria <adefitriafebrianti24@gmail.com>
2023-06-28 09:00:32 +0800

    upload pertama

---
+++ TabelKelas.pas
@@ -0,0 +1,196 @@
+unit TabelKelas;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, frxClass, frxDBSet, DB, ZAbstractRODataset, ZAbstractDataset,
+  ZDataset, ZAbstractConnection, ZConnection, StdCtrls, Grids, DBGrids;
+
+type
+  TForm1 = class(TForm)
+    l1: TLabel;
+    l2: TLabel;
+    l3: TLabel;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    b1: TButton;
+    b2: TButton;
+    b3: TButton;
+    b4: TButton;
+    b5: TButton;
+    dg1: TDBGrid;
+    c1: TComboBox;
+    b6: TButton;
+    ds1: TDataSource;
+    con1: TZConnection;
+    zqry1: TZQuery;
+    frxdbdtst1: TfrxDBDataset;
+    frxrprt1: TfrxReport;
+    procedure editbersih;
+    procedure editenable;
+    procedure posisiawal;
+    procedure b1Click(Sender: TObject);
+    procedure b2Click(Sender: TObject);
+    procedure b3Click(Sender: TObject);
+    procedure b4Click(Sender: TObject);
+    procedure b5Click(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure dg1CellClick(Column: TColumn);
+    procedure b6Click(Sender: TObject);
+    
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+  id : string;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.editbersih;
+begin
+Edit1.Clear;
+Edit2.Clear;
+c1.Clear
+end;
+
+procedure TForm1.editenable;
+begin
+edit1.Enabled:= True;
+edit2.Enabled:= True;
+c1.Enabled:= True;
+end;
+
+procedure TForm1.posisiawal;
+begin
+editbersih;
+
+edit1.Enabled:= False;
+edit2.Enabled:= False;
+c1.Enabled:= False;
+
+b1.Enabled:= True;
+b2.Enabled:= False;
+b3.Enabled:= False;
+b4.Enabled:= False;
+b5.Enabled:= False;
+end;
+
+procedure TForm1.b1Click(Sender: TObject);
+begin
+editbersih;
+
+b1.Enabled:= false;
+b2.Enabled:= True;
+b3.Enabled:= False;
+b4.Enabled:= False;
+b5.Enabled:= True;
+
+editenable;
+end;
+
+procedure TForm1.b2Click(Sender: TObject);
+begin
+if (Edit1.Text= '')or (Edit2.Text ='')or(c1.Text= '')then
+begin
+ShowMessage('DATA TIDAK BOLEH KOSONG!');
+end else
+if (zqry1.Locate('nama',Edit1.Text,[])) and (zqry1.Locate('jenis',Edit2.Text,[])) then
+begin
+ShowMessage('DATA KELAS SUDAH DIGUNAKAN');
+posisiawal;
+end else
+begin
+//simpan
+zqry1.SQL.Clear;
+zqry1.SQL.Add('insert into tabel_kelas values (null,"'+Edit1.Text+'","'+Edit2.Text+'","'+c1.Text+'")');
+zqry1.ExecSQL;
+
+zqry1.SQL.Clear;
+zqry1.SQL.Add('select * from tabel_kelas');
+zqry1.Open;
+ShowMessage('DATA BARHASIL DISIMPAN!');
+posisiawal;
+
+end;
+end;
+
+procedure TForm1.b3Click(Sender: TObject);
+begin
+if (Edit1.Text= '')or (Edit2.Text ='')or(c1.Text= '')then
+begin
+ShowMessage('INPUTAN WAJIB DIISI!');
+end else
+if (Edit1.Text = zqry1.Fields[1].AsString) and (Edit2.Text = zqry1.Fields[2].AsString) and (c1.Text = zqry1.Fields[3].AsString)then
+begin
+ShowMessage('DATA TIDAK ADA PERUBAHAN');
+posisiawal;
+end else
+begin
+id:=dg1.DataSource.DataSet.FieldByName('id_kelas').AsString;
+ShowMessage('DATA BERHASIL DIUPDATE!'); //UPDATE
+zqry1.SQL.Clear;
+zqry1.SQL.Add('Update tabel_kelas set nama= "'+Edit1.Text+'",jenis="'+Edit2.Text+'" where id_kelas="'+id+'"');
+zqry1. ExecSQL;
+
+zqry1.SQL.Clear;
+zqry1.SQL.Add('select * from tabel_kelas');
+zqry1.Open;
+posisiawal;
+end;
+end;
+
+procedure TForm1.b4Click(Sender: TObject);
+begin
+if MessageDlg('APAKAH YAKIN MENGHAPUS DATA INI?',mtWarning,[mbYes,mbNo],0)= mryes then
+begin
+id:=dg1.DataSource.DataSet.FieldByName('id_kelas').AsString;
+zqry1.SQL.Clear;
+zqry1.SQL.Add(' delete from tabel_kelas where id_kelas="'+id+'"');
+zqry1. ExecSQL;
+zqry1.SQL.Clear;
+zqry1.SQL.Add('select * from tabel_kelas');
+zqry1.Open;
+ShowMessage('DATA BERHASIL DIHAPUS');
+posisiawal;
+end else
+begin
+ShowMessage('DATA BATAL DIHAPUS');
+posisiawal;
+end;
+end;
+
+procedure TForm1.b5Click(Sender: TObject);
+begin
+posisiawal;
+end;
+
+procedure TForm1.FormShow(Sender: TObject);
+begin
+posisiawal;
+end;
+
+procedure TForm1.dg1CellClick(Column: TColumn);
+begin
+b1.Enabled:= true;
+b2.Enabled:= False;
+b3.Enabled:= True;
+b4.Enabled:= True;
+b5.Enabled:= True;
+id:=zqry1.Fields[0].AsString;
+Edit1.Text:= zqry1.FieldList[1].AsString;
+Edit2.Text:= zqry1.FieldList[2].AsString;
+c1.Text:= zqry1.FieldList[3].AsString;
+end;
+procedure TForm1.b6Click(Sender: TObject);
+begin
+frxrprt1.showreport();
+end;
+
+end.
